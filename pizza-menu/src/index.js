697e9ca4265d18418f7c2958c67dd2f1a3821f1f
maheswar <maheswarreddyappojigari@gmail.com>
2025-01-02 16:48:54 +0530

    pizza menu

---
+++ pizza-menu/src/index.js
@@ -0,0 +1,132 @@
+import { Fragment, StrictMode, useLayoutEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function Header() {
+  return (
+    <div className="header footer">
+      <h1>Pizza center</h1>
+    </div>
+  );
+}
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
+  return (
+    <li className="pizza">
+      <img src={photoName} alt="unable to render" />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}$</span>
+      </div>
+
+      <p>{soldOut}</p>
+    </li>
+  );
+}
+function Menu() {
+  let pizzaLength = pizzaData.length;
+
+  return (
+    <main className="menu">
+      <h2>OUR MENU</h2>
+      <p>
+        {" "}
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+      {pizzaLength > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((items) => (
+            <Pizza {...items} key={items.name} />
+          ))}
+        </ul>
+      ) : (
+        <h2>currently empty</h2>
+      )}
+    </main>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We are currently closed,We are open between {openHour}:00 and{" "}
+          {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+}
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We are open from {openHour} until {closeHour}.Come visit us or order
+        online
+      </p>
+      <button className="btn">ORDER NOW</button>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
